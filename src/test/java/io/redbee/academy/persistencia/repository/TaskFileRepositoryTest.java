294babae21d7f30668277bac05b198691b2c4c4f
Santi Leiva <santiago.leiva@redb.ee>
2021-09-02 23:18:18 -0300

    feature(tasks): agrega modelo de Tarea y repositorio
    Agrega pruebas unitarias.

---
+++ src/test/java/io/redbee/academy/persistencia/repository/TaskFileRepositoryTest.java
@@ -0,0 +1,51 @@
+package io.redbee.academy.persistencia.repository;
+
+import io.redbee.academy.persistencia.model.Task;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@DisplayName("TaskFileRepository test suite")
+public class TaskFileRepositoryTest {
+
+    @Disabled
+    @Test
+    @DisplayName("Get all tasks")
+    public void testGetAllTasks() {
+    }
+
+    @Test
+    @DisplayName("Save single task")
+    public void testSaveTask() {
+        Task task = new Task("Task description", LocalDate.now(), 1);
+
+        TaskRepository repository = new TaskFileRepository();
+
+        var exception = assertThrows(UnsupportedOperationException.class, () -> repository.save(task));
+
+        assertEquals("Requested operation is not supported", exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Save a list of tasks")
+    public void testSaveTasksList() {
+        TaskRepository repository = new TaskFileRepository();
+
+        List<Task> tasks = List.of(
+                new Task("Task 1", LocalDate.MAX, 1),
+                new Task("Task 2", LocalDate.MAX, 1),
+                new Task("Task 3", LocalDate.MAX, 1)
+        );
+
+        var exception = assertThrows(UnsupportedOperationException.class, () -> repository.save(tasks));
+
+        assertEquals("Requested operation is not supported", exception.getMessage());
+    }
+
+}
