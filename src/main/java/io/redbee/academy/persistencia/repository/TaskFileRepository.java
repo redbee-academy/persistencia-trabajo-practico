4db3ec61f100152cb51c8b42c5b3f8a11003a178
Santi Leiva <santiago.leiva@redb.ee>
2021-09-03 17:51:03 -0300

    test(tasks): agrega pruebas de TaskFileRepository

--- src/main/java/io/redbee/academy/persistencia/repository/TaskFileRepository.java
+++ src/main/java/io/redbee/academy/persistencia/repository/TaskFileRepository.java
@@ -1,21 +1,47 @@
 package io.redbee.academy.persistencia.repository;
 
 import io.redbee.academy.persistencia.model.Task;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.util.Collections;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDate;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class TaskFileRepository implements TaskRepository {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(TaskFileRepository.class);
+    private static final String DEFAULT_DATA_DELIMITER = ";";
+    private static final Long HEADER_ROW_SIZE = 1L;
+
     private final String filePath;
+    private final String dataDelimiter;
 
     public TaskFileRepository(String filePath) {
         this.filePath = filePath;
+        this.dataDelimiter = DEFAULT_DATA_DELIMITER;
+    }
+
+    public TaskFileRepository(String filePath, String dataDelimiter) {
+        this.filePath = filePath;
+        this.dataDelimiter = dataDelimiter;
     }
 
     @Override
     public List<Task> getAll() {
-        return Collections.emptyList();
+        try {
+            return Files.lines(Path.of(this.filePath))
+                    .skip(HEADER_ROW_SIZE)
+                    .map(fileLines -> fileLines.split(this.dataDelimiter))
+                    .map(this::toModel)
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            LOGGER.error("Error reading file: {}", e.getMessage(), e);
+            throw new RuntimeException("Error reading tasks file");
+        }
     }
 
     @Override
@@ -26,6 +52,13 @@ public class TaskFileRepository implements TaskRepository {
     @Override
     public void save(List<Task> tasks) {
         throw new UnsupportedOperationException("Requested operation is not supported");
+    }
+
+    private Task toModel(String[] taskFileLine) {
+        String description = taskFileLine[0];
+        LocalDate deadlineAsString = LocalDate.parse(taskFileLine[1]);
+        Integer employeeId = Integer.valueOf(taskFileLine[2]);
+        return new Task(description, deadlineAsString, employeeId);
     }
 
 }
--- src/test/java/io/redbee/academy/persistencia/repository/TaskFileRepositoryTest.java
+++ src/test/java/io/redbee/academy/persistencia/repository/TaskFileRepositoryTest.java
@@ -1,31 +1,45 @@
 package io.redbee.academy.persistencia.repository;
 
 import io.redbee.academy.persistencia.model.Task;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("TaskFileRepository test suite")
 public class TaskFileRepositoryTest {
 
-    @Disabled
     @Test
-    @DisplayName("Get all tasks")
-    public void testGetAllTasks() {
+    @DisplayName("Get all tasks ok")
+    public void testGetAllTasksOk() {
+        TaskFileRepository repository = new TaskFileRepository("./src/test/resources/valid-3-tasks.csv");
+
+        List<Task> tasks = assertDoesNotThrow(repository::getAll);
+
+        assertNotNull(tasks);
+
+        assertEquals(3, tasks.size());
+    }
+
+    @Test
+    @DisplayName("Get all task throws an exception")
+    public void testGetAllTasksThrowsAnException() {
+        TaskFileRepository repository = new TaskFileRepository("some/fake/path/throws/error.csv");
+
+        var exception = assertThrows(RuntimeException.class, repository::getAll);
+
+        assertEquals("Error reading tasks file", exception.getMessage());
     }
 
     @Test
     @DisplayName("Save single task")
     public void testSaveTask() {
-        Task task = new Task("Task description", LocalDate.now(), 1);
+        Task task = new Task("Task description", LocalDate.MAX, 1);
 
-        TaskRepository repository = new TaskFileRepository("some/fake/path.csv");
+        TaskRepository repository = new TaskFileRepository("./src/test/resources/valid-3-tasks.csv");
 
         var exception = assertThrows(UnsupportedOperationException.class, () -> repository.save(task));
 
@@ -35,7 +49,7 @@ public class TaskFileRepositoryTest {
     @Test
     @DisplayName("Save a list of tasks")
     public void testSaveTasksList() {
-        TaskRepository repository = new TaskFileRepository("some/fake/path.csv");
+        TaskRepository repository = new TaskFileRepository("./src/test/resources/valid-3-tasks.csv");
 
         List<Task> tasks = List.of(
                 new Task("Task 1", LocalDate.MAX, 1),
